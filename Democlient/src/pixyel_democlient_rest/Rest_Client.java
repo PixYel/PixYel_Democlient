d2b706d5bc8520e85d7f017ec60a71fe32e8a69e
Josua Frank <Josua Frank@192.168.178.128>
2016-12-10 09:40:13 +0100

    HUGE update, implemented working Rest!!

--- Democlient/src/pixyel_democlient_rest/Rest_Client.java
+++ Democlient/src/pixyel_democlient_rest/Rest_Client.java
@@ -6,19 +6,14 @@
 package pixyel_democlient_rest;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import pixyel_democlient_rest.encryption.Encryption;
 import pixyel_democlient_rest.xml.XML;
 
@@ -28,7 +23,7 @@ import pixyel_democlient_rest.xml.XML;
  */
 public class Rest_Client {
 
-    String serverIP = "sharknoon.de";//IP-Adresse des Servers, zum testes localhost (Server und Client auf dem selben Computer), wird später "sharknoon.de" sein!
+    String serverIP = "localhost";//IP-Adresse des Servers, zum testes localhost (Server und Client auf dem selben Computer), wird später "sharknoon.de" sein!
     //Der öffentliche Key des Servers
     String serverPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAmG8OfhJrkN9/rXLh7auyUPcq7UxmYModYswChY8hIMgZO4m+cxOWopxOptUAYedjA4ZAKGp/P1g6n6YaXvtPQqIbi7G5oCT4vbh0zYFgI3wNCJlKtUX1gb6uCQW3rPinANcPtlZoIyegAsn/OW0FMZtc1x8PN0H1MQTlcCctXdJdotuljeYriO1lkRfb3GsotLIYjciMqIMKGQRQ2Rhj81bnxP9FybdNuVIjlS6Rfx9fzaZ2BKIdm7O7/Dzn9TcSZEOZdOSS7CHMMKr14O26g+bR2HiGWx8AbOH2zP3DMpR9/Y8GUrjO6QPqA+GorICGYWxIlrcm4iYx8740FsDaQQIDAQAB";
     //Der private Key des Clients
@@ -43,11 +38,11 @@ public class Rest_Client {
     }
 
     public Rest_Client() throws InterruptedException {
-        //**Verbindung zum Server herstellen ENTWEDER Ping ODER connect aufrufen!!!!!!!!!!!11!!!!!elf!!
         login("blob");
         //**Beispiel: sendet ein xml mit dem node "echo" an den Server, der server schickt daraufhin selbiges zurück
-        sendToServer(XML.createNewXML("request").addChild("echo").toString());
+        sendToServer(XML.createNewXML("request").addAttribute("storeId", storeId).addChild("echo").toString());
         //**Wenn man die App schließt oder ähnliches, einfach die disconnect Methode aufrufen
+        sendToServer(XML.createNewXML("request").addAttribute("storeId", storeId).addChild("getItem").addChild("id").setContent("1").toString());
         Thread.sleep(1000);
         disconnect();
     }
@@ -60,35 +55,23 @@ public class Rest_Client {
             HttpPost httppost = new HttpPost("http://" + serverIP + ":7332/api/request/");
 
             // Request parameters and other properties.
-            List<NameValuePair> params = new ArrayList<>();
-            params.add(new BasicNameValuePair("storeId", storeId));
-            params.add(new BasicNameValuePair("xml", encrypted));
-            httppost.setEntity(new UrlEncodedFormEntity(params));
+            httppost.setEntity(new StringEntity(encrypted));
             //Execute and get the response.
             HttpResponse response = httpclient.execute(httppost);
-            HttpEntity entity = response.getEntity();
+            String received = EntityUtils.toString(response.getEntity());
 
-            if (entity != null) {
-                InputStream instream = entity.getContent();
-                try {
-                    java.util.Scanner s = new java.util.Scanner(instream).useDelimiter("\\A");
-                    String received = s.hasNext() ? s.next() : "";
-                    if (!received.isEmpty()) {
-                        String decrypted = Encryption.decrypt(received, clientPrivateKey);
-                        System.out.println(decrypted);
-                    }
-                } finally {
-                    instream.close();
-                }
+            if (received != null && !received.isEmpty()) {
+                String decrypted = Encryption.decrypt(received, clientPrivateKey);
+                System.out.println(XML.openXML(decrypted).toStringGraph());
             }
 
-        } catch (IOException | Encryption.EncryptionException ex) {
-            Logger.getLogger(Rest_Client.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException | Encryption.EncryptionException | XML.XMLException ex) {
+            System.err.println("Could not send String to Server: " + ex);
         }
     }
 
     private void disconnect() {
-        sendToServer(XML.createNewXML("request").addChild("disconnect").toString());
+        sendToServer(XML.createNewXML("request").addAttribute("storeId", storeId).addChild("disconnect").toString());
         System.out.println("Habe mich beim Server abgemeldet und beende mich jetzt...");
         System.exit(0);
     }
@@ -104,7 +87,7 @@ public class Rest_Client {
             //Speichere den Private Key für andere Methoden sichtbar
             clientPrivateKey = keyPair[1];
             //Erzeuge ein XML mit einem Tag namens publickey und einem tag namens storeid, siehe Spezifikation!
-            XML loginXML = XML.createNewXML("request").addChild("login");
+            XML loginXML = XML.createNewXML("request").addAttribute("storeId", storeId).addChild("login");
             loginXML.addChildren("storeId", "publicKey");
             loginXML.getFirstChild("storeId").setContent(storeId);
             loginXML.getFirstChild("publicKey").setContent(keyPair[0]);
